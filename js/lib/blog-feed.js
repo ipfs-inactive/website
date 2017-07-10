4fd21836163dd5095d89e4333d924d3a50167c00
Richard Silverton <silverton.richard@googlemail.com>
2017-07-10 17:49:31 +0100

    Include blog postts in build process

--- js/lib/blog-feed.js
+++ js/lib/blog-feed.js
@@ -2,6 +2,7 @@ var $ = require('jquery')
 
 function renderPosts (data) {
   var $container = $('#latest-list')
+  $container.empty()
   data.posts.slice(0, 4).forEach(function (post) {
     var $el = $('<li><span class="date">' + post.date + '</span> <a href="' + post.url + '">' + post.title + '</a></li>')
     $container.append($el)
--- js/lib/popup.js
+++ js/lib/popup.js
@@ -1,13 +1,15 @@
 var $ = require('jquery')
 require('magnific-popup')
 
-$(document).ready(function () {
-  $('.popup-youtube').magnificPopup({
-    disableOn: 700,
-    type: 'iframe',
-    mainClass: 'mfp-fade',
-    removalDelay: 160,
-    preloader: false,
-    fixedContentPos: false
+module.exports = function () {
+  $(document).ready(function () {
+    $('.popup-youtube').magnificPopup({
+      disableOn: 700,
+      type: 'iframe',
+      mainClass: 'mfp-fade',
+      removalDelay: 160,
+      preloader: false,
+      fixedContentPos: false
+    })
   })
-})
+}
