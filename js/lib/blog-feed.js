95b96cf087c0beb7093c5944645a7cea6c814f8b
Alan Shaw <alan@tableflip.io>
2017-08-05 14:39:19 +0100

    fixes #220 Prefix /blog to blog post URL (#221)
    Since the blog site is mounted at ipfs.io/blog it's appropriate to add this detail here and not in the blog site i.e. the blog site makes no assumptions about where's it's mounted

--- js/lib/blog-feed.js
+++ js/lib/blog-feed.js
@@ -4,7 +4,7 @@ function renderPosts (data) {
   var $container = $('#latest-list')
   $container.empty()
   data.posts.slice(0, 4).forEach(function (post) {
-    var $el = $('<li><span class="date">' + post.date + '</span> <a href="' + post.url + '">' + post.title + '</a></li>')
+    var $el = $('<li><span class="date">' + post.date + '</span> <a href="/blog' + post.url + '">' + post.title + '</a></li>')
     $container.append($el)
   })
 }
