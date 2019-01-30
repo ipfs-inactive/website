0225fa82bb51e16910adaab11085639b65de9c7c
Portia Burton <plburton@gmail.com>
2019-01-30 11:28:50 +0100

    Minor verbiage edits

--- js/lib/alert-bar.js
+++ js/lib/alert-bar.js
@@ -6,7 +6,7 @@ function callTime () {
   var startTime = parseInt(callData.time, 10)
   var dayOfWeek = new Date().getDay()
   var reminder = $('.alert-bar')
-  var announcement = $('.lh-title').append('<span>The IPFS community call will start in ' + callData.time + '. Join us <a href=' + callData.zoomLink + '>here</a></span>')
+  var announcement = $('.lh-title').append('<span>The IPFS community call will start at ' + callData.time + '. Join us <a href=' + callData.zoomLink + '>here</a></span>')
   if ((startTime - now <= 2 && startTime - now >= 0) && (dayOfWeek === 1)) {
     announcement()
   } else {
