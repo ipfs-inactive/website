30184c01efc1358fd9f014928478ed90450d2a71
Portia Burton <plburton@gmail.com>
2019-01-30 10:35:43 +0100

    Alert bar will only appear on Mondays

--- js/lib/alert-bar.js
+++ js/lib/alert-bar.js
@@ -4,9 +4,10 @@ var callData = require('./communityCall.json')
 function callTime () {
   var now = new Date().getHours()
   var startTime = parseInt(callData.time, 10)
+  var dayOfWeek = new Date().getDay()
   var reminder = $('.alert-bar')
   var announcement = $('.lh-title').append('<span>The IPFS community call will start in ' + callData.time + '. Join us <a href=' + callData.zoomLink + '>here</a></span>')
-  if (now - startTime <= 2) {
+  if ((now - startTime <= 2 && now - startTime >= 0) && (dayOfWeek === 1)) {
     announcement()
   } else {
     reminder.hide()
