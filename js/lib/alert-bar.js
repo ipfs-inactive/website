158c1ffd417f835cc7c199a899ec0fab8a82cfd7
Portia Burton <plburton@gmail.com>
2019-02-20 18:14:15 +0100

    Alert bar only appears between 16pm-18:30pm UTC

--- js/lib/alert-bar.js
+++ js/lib/alert-bar.js
@@ -2,15 +2,15 @@ var $ = require('jquery')
 var callData = require('./communityCall.json')
 
 function callTime () {
-  var now = new Date().getHours()
+  var now = new Date().getUTCHours()
   var startTime = parseInt(callData.time, 10)
-  var dayOfWeek = new Date().getDay()
+  var dayOfWeek = new Date().getUTCDay()
   var reminder = $('.alert-bar')
   var announcement = $('.lh-title').append('<span>The IPFS community call will start at ' + callData.time + '. Join us <a href=' + callData.zoomLink + '>here</a></span>')
-  if ((startTime - now <= 2 && startTime - now >= 0) && (dayOfWeek === 1)) {
+  if ((startTime - now <= 2 && startTime - now >= 0.5) && (dayOfWeek === callData.day)) {
     announcement()
   } else {
-    reminder.hide()
+    reminder.css('display', 'none')
   }
 }
 callTime()
