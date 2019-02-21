4c293b25a27770b75477dae700b510b6578ea52c
Portia Burton <plburton@gmail.com>
2019-02-21 12:32:40 +0100

    Alert bar does not show by default

--- js/lib/alert-bar.js
+++ js/lib/alert-bar.js
@@ -6,11 +6,11 @@ function callTime () {
   var startTime = parseInt(callData.time, 10)
   var dayOfWeek = new Date().getUTCDay()
   var reminder = $('.alert-bar')
-  var announcement = $('.lh-title').append('<span>The IPFS community call will start at ' + callData.time + '. Join us <a href=' + callData.zoomLink + '>here</a></span>')
   if ((startTime - now <= 2 && startTime - now >= 0.5) && (dayOfWeek === callData.day)) {
-    announcement()
+    $('.alert-bar-message').append('<span>The IPFS community call will start at ' + callData.time + '. Join us <a href=' + callData.zoomLink + '>here</a></span>')
+    reminder.show()
   } else {
-    reminder.css('display', 'none')
+    reminder.hide()
   }
 }
 callTime()
