7ee5478d744f3944482a5bf58141e0e052087134
Chris Waring <chris@wwaves.co>
2019-08-15 15:50:41 +0100

    fix standard formatting

--- js/lib/get-started.js
+++ js/lib/get-started.js
@@ -1,11 +1,11 @@
 var $ = require('jquery')
 
-module.exports = function() {
-  $(document).ready(function() {
+module.exports = function () {
+  $(document).ready(function () {
     var ctx = '.option-blocks'
-    $('input[type=checkbox]', ctx).on('click', function() {
+    $('input[type=checkbox]', ctx).on('click', function () {
       var selected = this
-      $('input[type=checkbox]:checked', ctx).each(function() {
+      $('input[type=checkbox]:checked', ctx).each(function () {
         if (this !== selected) $(this).prop('checked', false)
       })
     })
