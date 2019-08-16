80a7aaef63cddb4ca57d26b7ba993cd777dab872
Chris Waring <chris@wwaves.co>
2019-08-16 20:45:13 +0100

    sketch out event tracking for sections and links

--- js/lib/get-started.js
+++ js/lib/get-started.js
@@ -1,12 +1,39 @@
 var $ = require('jquery')
 
 module.exports = function () {
+  var ctx = '.option-blocks'
+  var lastOption = null
+
+  function trackOutboundLinkClick (event) {
+    if (!window.ga) return
+    ga('send', 'event', {
+      eventCategory: 'outbound link',
+      eventAction: 'click',
+      eventLabel: event.target.href
+    })
+  }
+  function trackOptionClick (el) {
+    if (!window.ga) return
+    var title = $(el).closest('.option-block').find('h4').text().toLowerCase()
+    ga('send', 'event', {
+      eventCategory: el.checked ? 'open option' : 'close option',
+      eventAction: 'click',
+      eventLabel: title
+    })
+  }
+
   $(document).ready(function () {
-    var ctx = '.option-blocks'
+    // setup outbound link tracking for all of the homepage
+    $('a').on('click', function (event) {
+      trackOutboundLinkClick(event)
+    })
+    // setup click action on option section
     $('input[type=checkbox]', ctx).on('click', function () {
-      var selected = this
+      lastOption = this
+      trackOptionClick(lastOption)
+      // close all other option options
       $('input[type=checkbox]:checked', ctx).each(function () {
-        if (this !== selected) $(this).prop('checked', false)
+        if (this !== lastOption) $(this).prop('checked', false)
       })
     })
   })
